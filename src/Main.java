8700051631ff7ff12de693c8e12a6a15ad4bea64
unknown <>
2017-05-09 01:58:12 +0900

    Java Operators

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by 임승현 on 2017-05-09.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello");
+
+        // Operators
+        Operators operators = new Operators();
+        operators.calculate();
+    }
+}
---
+++ src/Operators.java
@@ -0,0 +1,18 @@
+/**
+ * Created by 임승현 on 2017-05-09.
+ */
+public class Operators {
+    public void calculate() {
+        System.out.println(1 + 2);
+        System.out.println(3 - 1);
+        System.out.println(5 * 4);
+        System.out.println(6 / 2);
+
+        // 몫을 구하는 연산자.
+        System.out.println(5 / 2);
+
+        // 나머지를 구하는 연산자.
+        System.out.println(5 % 2);
+
+    }
+}

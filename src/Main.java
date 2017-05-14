e129402f04f92f0a121d0f5e1651de8077bc86e6
unknown <>
2017-05-14 14:37:22 +0900

    Update Main.class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by 임승현 on 2017-05-09.
  */
@@ -10,16 +12,29 @@ public class Main {
         //operators.calculate();
 
         // Data Types
-        DataTypes dataTypes = new DataTypes();
+        //DataTypes dataTypes = new DataTypes();
         //dataTypes.explainInteger();
         //dataTypes.explainDouble();
         //dataTypes.castNumber();
         //dataTypes.explainString();
         //dataTypes.parseData();
-        CollectionFrameworks collectionFrameworks = new CollectionFrameworks();
+        //CollectionFrameworks collectionFrameworks = new CollectionFrameworks();
         //collectionFrameworks.explainArray();
         //collectionFrameworks.explainArrayList();
         //collectionFrameworks.explainHashSet();
-        collectionFrameworks.explainHashMap();
+        //collectionFrameworks.explainHashMap();
+
+        Project02 project02 = new Project02(4,5);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫번째 수를 입력하세요.");
+        project02.a=sc.nextDouble();
+        System.out.println("두번째 수를 입력하세요.");
+        project02.b=sc.nextDouble();
+
+        System.out.println(project02.sum());
+        System.out.println(project02.sub());
+        System.out.println(project02.multply());
+        System.out.println(project02.divsion());
+
     }
 }
